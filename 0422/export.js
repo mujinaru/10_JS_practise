1ef958ebd721862d2194efd876f4e0f59736cf4d
sibata fuuga <m.fuuga720@gmail.com>
2024-04-22 12:51:55 +0900

    0422
    クラス・インポート＆エクスポート・天気予報API

---
+++ 0422/export.js
@@ -0,0 +1,20 @@
+
+//<h1>エクスポート（書き出し）</h1>
+
+//変数myNumberをエクスポート
+export const myNumber = 2;
+
+//オブジェクトmyObjectをエクスポート
+export const myObject = { name: "渡来", age: 18 };
+
+//関数myfunctionをエクスポート
+export function myFunction() {
+    console.log("my task");
+}
+
+//クラスmyClassをエクスポート
+export class myClass {
+    constructor() { }
+}
+
+//エクスポートされたjsはモジュールになる
---
+++ 0422/import.js
@@ -0,0 +1,8 @@
+//JavaScriptファイルからモジュールを読み込む
+import { myNumber, myObject } from "./export.js";
+
+console.log(myNumber);
+console.log(myObject);
+
+
+//インポート側もモジュールになる
